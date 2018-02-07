400d56d73b61da38a3414b40e1b306d2a2af5075
Вакуленко Алексей <vakulenko_alex@mail.ru>
2018-02-07 14:19:18 +0400

    добавил возможность обновления базы после загрузки

--- runner1c/commands/load_config.py
+++ runner1c/commands/load_config.py
@@ -17,12 +17,16 @@ class LoadConfigParser(runner1c.parser.Parser):
     def set_up(self):
         self.add_argument_to_parser()
         self._parser.add_argument('--folder', required=True, help='каталог, содержащий XML-файлы конфигурации')
-
+        self._parser.add_argument('--update', action='store_const', const=True, help='обновление конфигурации '
+                                                                                     'базы данных')
 
 class LoadConfig(runner1c.command.Command):
     def execute(self):
         builder = runner1c.cmd_string.CmdString(mode=runner1c.cmd_string.Mode.DESIGNER, parameters=self._parameters)
         builder.add_string('/LoadConfigFromFiles "{folder}"')
+
+        if getattr(self._parameters, 'update', False):
+            builder.add_string('/UpdateDBCfg')
 
         setattr(self._parameters, 'cmd', builder.get_string())
         return self.start()
