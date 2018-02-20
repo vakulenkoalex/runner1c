8ddb2b11bce2d5133db738f9b78fa48a0a84610b
Вакуленко Алексей <vakulenko_alex@mail.ru>
2018-02-20 20:01:42 +0400

    запуск в режиме предприятия идет без ожидания результата

--- runner1c/commands/base_for_test.py
+++ runner1c/commands/base_for_test.py
@@ -31,8 +31,6 @@ class CreateBaseForTest(runner1c.command.Command):
     def execute(self):
         folder_for_config_src = 'cf'
 
-        #todo после создания базы можно паралельно создавать обработки и обновлять базу в режиме Предприятия
-
         p_create = runner1c.command.EmptyParameters(self.arguments)
         setattr(p_create, 'connection', self.arguments.connection)
         return_code = runner1c.command.CreateBase(arguments=p_create).execute()
@@ -41,25 +39,27 @@ class CreateBaseForTest(runner1c.command.Command):
 
             self.start_agent()
 
-            command = 'config load-files --dir "{}\\{}" --update-config-dump-info'
-            return_code = self.send_to_agent(command.format(self.arguments.folder, folder_for_config_src))
-            if exit_code.success_result(return_code):
-                return_code = self.send_to_agent('config update-db-cfg')
+            try:
+                command = 'config load-files --dir "{}\\{}" --update-config-dump-info'
+                return_code = self.send_to_agent(command.format(self.arguments.folder, folder_for_config_src))
                 if exit_code.success_result(return_code):
-                    if getattr(self.arguments, 'create_epf', False):
-                        p_sync = runner1c.command.EmptyParameters(self.arguments)
-                        setattr(p_sync, 'connection', self.arguments.connection)
-                        setattr(p_sync, 'folder', self.arguments.folder)
-                        setattr(p_sync, 'create', True)
-                        return_code = sync.Sync(arguments=p_sync, agent_channel=self.get_agent_channel()).execute()
+                    return_code = self.send_to_agent('config update-db-cfg')
 
-            self.close_agent()
+                    if exit_code.success_result(return_code):
+                        p_start = runner1c.command.EmptyParameters(self.arguments)
+                        setattr(p_start, 'connection', self.arguments.connection)
+                        setattr(p_start, 'thick', self.arguments.thick)
+                        setattr(p_start, 'no_wait', True)
+                        setattr(p_start, 'epf', common.get_path_to_project('build\\tools\\epf\\CloseAfterUpdate.epf'))
+                        runner1c.commands.start.Start(arguments=p_start).execute()
 
-            if exit_code.success_result(return_code):
-                p_start = runner1c.command.EmptyParameters(self.arguments)
-                setattr(p_start, 'connection', self.arguments.connection)
-                setattr(p_start, 'thick', self.arguments.thick)
-                setattr(p_start, 'epf', common.get_path_to_project('build\\tools\\epf\\CloseAfterUpdate.epf'))
-                return_code = runner1c.commands.start.Start(arguments=p_start).execute()
+                        if getattr(self.arguments, 'create_epf', False):
+                            p_sync = runner1c.command.EmptyParameters(self.arguments)
+                            setattr(p_sync, 'connection', self.arguments.connection)
+                            setattr(p_sync, 'folder', self.arguments.folder)
+                            setattr(p_sync, 'create', True)
+                            return_code = sync.Sync(arguments=p_sync, agent_channel=self.get_agent_channel()).execute()
+            finally:
+                self.close_agent()
 
         return return_code
