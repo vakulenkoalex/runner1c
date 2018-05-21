6fb9a0e45d1dba79cb531e562e98479cc90e5c00
Вакуленко Алексей <vakulenko_alex@mail.ru>
2018-05-21 17:24:45 +0400

    добавил тест diff_mxl

---
+++ tests/test_diff_mxl.py
@@ -0,0 +1,23 @@
+
+import os
+
+import pytest
+
+
+def count_lines_in_file(file):
+    with open(file, mode='r', encoding='utf-8') as file_stream:
+        count = len(file_stream.readlines())
+    file_stream.close()
+
+    return count
+
+
+@pytest.mark.usefixtures("set_log_level")
+def test_diff_same_mxl(runner, tmpdir, repo_folder):
+    first = os.path.join(repo_folder, 'Проверка.mxl')
+    second = first
+    result = str(tmpdir.join("result.txt"))
+    argument = ['--debug', 'diff_mxl', '--first', first, '--second', second, '--result', result]
+    assert runner(argument) == 0
+
+    assert count_lines_in_file(result) == 1
