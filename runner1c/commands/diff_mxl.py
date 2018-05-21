1e01b7a820cc9653c946ff28b8ae30a116aa6e58
Вакуленко Алексей <vakulenko_alex@mail.ru>
2018-05-21 17:23:31 +0400

    добавил получение результата сравнения

--- runner1c/commands/diff_mxl.py
+++ runner1c/commands/diff_mxl.py
@@ -17,15 +17,20 @@ class DiffMxlParser(runner1c.parser.Parser):
         return DiffMxl(**kwargs)
 
     def set_up(self):
-        self._parser.add_argument('--first', required=True, help='путь к первому файлу')
-        self._parser.add_argument('--second', required=True, help='путь ко второму файлу')
+        self._parser.add_argument('--first', required=True, help='путь к списку файлов')
+        self._parser.add_argument('--second', required=True, help='путь к списку файлов')
+        self._parser.add_argument('--result', help='путь к списку файлов, которые совпали')
 
 
 class DiffMxl(runner1c.command.Command):
     def execute(self):
+        options = 'First={};Second={}'.format(self.arguments.first, self.arguments.second)
+        if getattr(self.arguments, 'result', False):
+            options = '{};Result={}'.format(options, self.arguments.result)
+
         p_start = runner1c.command.EmptyParameters(self.arguments)
         setattr(p_start, 'thick', True)
         setattr(p_start, 'epf', common.get_path_to_project('build\\tools\\epf\\FileCompareMxl.epf'))
-        setattr(p_start, 'options', 'First={};Second={}'.format(self.arguments.first, self.arguments.second))
+        setattr(p_start, 'options', options)
 
         return runner1c.commands.start.Start(arguments=p_start).execute()
