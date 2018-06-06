dc1748d16c1aed8943d94ceabf448f7a77ca726f
Вакуленко Алексей <vakulenko_alex@mail.ru>
2018-06-06 10:21:40 +0400

    добавил тест для проверки работы sync без базы

---
+++ tests/test_sync_without_base.py
@@ -0,0 +1,26 @@
+import os
+
+import pytest
+
+
+@pytest.mark.usefixtures("set_log_level")
+def test_sync_without_base(runner):
+    test_dir = os.path.dirname(__file__)
+    repo_folder = os.path.join(test_dir, 'repo')
+
+    epf = os.path.join(repo_folder, 'build', 'epf', 'ПроверитьКонфигурацию.epf')
+    if os.path.exists(epf):
+        os.remove(epf)
+    # обработки нет
+    assert not os.path.exists(epf)
+
+    argument = ['--debug',
+                'sync',
+                '--folder',
+                repo_folder,
+                '--create']
+    assert runner(argument) == 0
+
+    # обработка создалась
+    assert os.path.exists(epf)
+
