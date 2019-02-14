2e50e861a4ea404c74def0eb9962f3ba1ec55369
Вакуленко Алексей <vakulenko_alex@mail.ru>
2019-02-14 17:10:28 +0400

    вывел путь к исходникам на форму добавил загрузку расширений в базу

--- runner1c/tools/create_base_for_test.py
+++ runner1c/tools/create_base_for_test.py
@@ -6,7 +6,6 @@ import tkinter.messagebox as messagebox
 from configparser import ConfigParser
 
 import runner1c
-import runner1c.common as common
 
 
 def _ask_directory_click(element, title):
@@ -32,24 +31,24 @@ def _place_ask_directory(label_text, position):
     return entry
 
 
-def place_checkbox(label_text, position):
+def place_checkbox(label_text, x, y):
     var = tkinter.IntVar()
     checkbutton = tkinter.Checkbutton(FORM, text=label_text, variable=var)
-    checkbutton.place(x=90 + position, y=90)
+    checkbutton.place(x=x, y=y)
 
     return var
 
 
-INI_FILE = common.get_path_to_project(os.path.join('build', 'base_for_test.ini'))
+INI_FILE = os.path.join(os.getenv('USERPROFILE'), 'base_for_test.ini')
 CONFIG = ConfigParser()
 CONFIG.read(INI_FILE)
 FORM = tkinter.Tk()
 BASE = _place_ask_directory('Путь к базе', 0)
 PLATFORM = _place_ask_directory('Путь к платформе', 40)
-CREATE_EPF = place_checkbox('Создать epf', 0)
-#CREATE_CFE = place_checkbox('Создать cfe', 90)
-REPO = tkinter.StringVar()
-PATH_GIT = os.path.split(os.path.split(os.getcwd())[0])[0]
+REPO = _place_ask_directory('Путь к исходникам', 80)
+THICK_CLIENT = place_checkbox('Толстый клиент', 90, 130)
+CREATE_EPF = place_checkbox('Создать epf', 210, 130)
+CREATE_CFE = place_checkbox('Создать cfe', 300, 130)
 
 
 def _set_entry_value(element, text):
@@ -60,29 +59,43 @@ def _delete_entry_value(element):
     element.delete(0, tkinter.END)
 
 
-def _radiobutton_change():
+def _radiobutton_change(repo_name):
     _delete_entry_value(BASE)
     _delete_entry_value(PLATFORM)
+    _delete_entry_value(REPO)
+    THICK_CLIENT.set(False)
     # noinspection PyPep8,PyBroadException
     try:
-        _set_entry_value(BASE, CONFIG.get(REPO.get(), 'base'))
-        _set_entry_value(PLATFORM, CONFIG.get(REPO.get(), 'platform'))
+        _set_entry_value(BASE, CONFIG.get(repo_name, 'base'))
+        _set_entry_value(PLATFORM, CONFIG.get(repo_name, 'platform'))
+        _set_entry_value(REPO, CONFIG.get(repo_name, 'repo'))
+        THICK_CLIENT.set(CONFIG.get(repo_name, 'thick_client'))
     except:
         pass
 
 
 def _place_repo(position, key):
+    repo_name = CONFIG.get(key, 'name')
     rbutton = tkinter.Radiobutton(FORM,
-                                  text=CONFIG.get(key, 'name'),
+                                  text=repo_name,
                                   value=key,
                                   variable=REPO,
-                                  command=lambda: _radiobutton_change())
+                                  command=lambda: _radiobutton_change(repo_name))
     rbutton.place(x=280, y=20 * position)
 
 
-def _save_parameters(repo_name, base_path, platform_path):
+def _save_parameters(repo_path, base_path, platform_path, thick_client):
+    repo_name = os.path.split(repo_path)[1]
+
+    if not CONFIG.has_section(repo_name):
+        CONFIG.add_section(repo_name)
+
+    CONFIG.set(repo_name, 'name', repo_name)
+    CONFIG.set(repo_name, 'repo', repo_path)
     CONFIG.set(repo_name, 'base', base_path)
     CONFIG.set(repo_name, 'platform', platform_path)
+    CONFIG.set(repo_name, 'thick_client', str(thick_client))
+
     with open(INI_FILE, 'w') as file:
         CONFIG.write(file)
     file.close()
@@ -103,30 +116,33 @@ def _create_base_click():
         messagebox.showerror("Ошибка", 'Не указан путь к платформе')
         return
 
+    if not REPO.get():
+        messagebox.showerror("Ошибка", 'Не указан путь к исходникам')
+        return
+
     if os.path.exists(BASE.get()):
         shutil.rmtree(BASE.get(), True)
+    os.makedirs(BASE.get())
 
-    repo_path = os.path.join(PATH_GIT, REPO.get())
     argument = ['--debug', 'base_for_test', '--silent', '--path', PLATFORM.get(), '--connection', 'File=' + BASE.get(),
-                '--folder', repo_path]
+                '--folder', REPO.get()]
     if CREATE_EPF.get():
         argument.append('--create_epf')
-    # noinspection PyBroadException,PyPep8
-    try:
-        if CONFIG.get(REPO.get(), 'thick'):
-            argument.append('--thick')
-    except:
-        pass
+    if CREATE_CFE.get():
+        argument.append('--create_cfe')
+    if THICK_CLIENT.get():
+        argument.append('--thick')
 
-    _save_parameters(REPO.get(), BASE.get(), PLATFORM.get())
-    #_save_git_path_for_base(BASE.get(), repo_path)
+    _save_parameters(REPO.get(), BASE.get(), PLATFORM.get(), THICK_CLIENT.get())
+    _save_git_path_for_base(BASE.get(), REPO.get())
 
-    runner1c.core.main(argument)
+    if runner1c.core.main(argument) == 0:
+        FORM.destroy()
 
 
 def _create_form():
     FORM.title('Создание базы для тестов')
-    FORM.geometry('430x130')
+    FORM.geometry('430x160')
 
     i = 0
     for key in CONFIG.sections():
@@ -136,7 +152,7 @@ def _create_form():
     create_button = tkinter.Button(FORM,
                                    text='Создать базу',
                                    command=lambda: _create_base_click())
-    create_button.place(x=5, y=90)
+    create_button.place(x=5, y=130)
 
     FORM.mainloop()
 
